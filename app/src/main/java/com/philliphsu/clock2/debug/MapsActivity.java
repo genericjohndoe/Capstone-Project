a5ceef3fbd81460e688ba049cdeb35138ff1105d
genericjohndoe <j.robinson.johnson21@gmail.com>
2017-04-27 19:34:00 -0500

    mapactivity updated to not have string

--- app/src/main/java/com/philliphsu/clock2/debug/MapsActivity.java
+++ app/src/main/java/com/philliphsu/clock2/debug/MapsActivity.java
@@ -3,12 +3,9 @@ package com.philliphsu.clock2.debug;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.philliphsu.clock2.R;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -40,8 +37,5 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
